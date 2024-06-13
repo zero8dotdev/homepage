dd0d22c31a894742a3351f53cefade4a5973d3a4
Ashutosh Tripathi <hello@zero8.dev>
2024-06-13 20:05:17 +0530

    first commit

---
+++ src/app/_components/header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="h-[64px] bg-white px-8 flex justify-center items-center border-[1px] fixed w-full">
+      <a className="text-[#cc4230]">&lt;zero8.dev&#47;&gt;</a>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/app/_components/sections.tsx
@@ -0,0 +1,42 @@
+const First = () => {
+  return (
+    <div className="h-screen flex justify-center items-center text-center">
+      <h1 className="text-black text-[100px]">
+        Our website isn&apos;t ready yet. Why?
+      </h1>
+    </div>
+  );
+};
+
+const Second = () => {
+  return (
+    <div className="h-screen flex justify-center items-center text-center">
+      <h2 className="text-black text-[80px]">
+        Honestly, we were hoping you wouldn&apos;t notice.
+      </h2>
+    </div>
+  );
+};
+
+const Third = () => {
+  return (
+    <div className="h-screen flex justify-center items-center text-center">
+      <h3 className="text-[60px]">
+        But since you&apos;re here, we promise it&apos;ll be worth the wait.
+      </h3>
+    </div>
+  );
+};
+
+const Fourth = () => {
+  return (
+    <div className="h-screen flex justify-center items-center text-center">
+      <h4 className="text-[40px]">
+        Unless it isn&apos;t, in which case, blame the intern.
+      </h4>
+      ;
+    </div>
+  );
+};
+
+export { First, Second, Third, Fourth };
