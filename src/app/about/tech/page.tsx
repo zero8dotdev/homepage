1b6afcdb36fad2edba1c75b53cccf4c30d9177e5
Akash <akash.v.harale@gmail.com>
2025-03-20 17:42:13 +0530

    feat : about/tech page

---
+++ src/app/about/tech/page.tsx
@@ -0,0 +1,260 @@
+import Image from "next/image";
+import React from "react";
+
+const AboutTech = () => {
+  return (
+    <div className="col-span-full">
+      {/* main heading Tech Stack */}
+      <div>
+        <h1 className="primary-heading ">Tech Stack</h1>
+      </div>
+      {/* Flutter */}
+      <div className="grid gap-5 lg:grid-cols-2">
+        {/* flutter logo */}
+        <div className="col-span-2">
+          <Image
+            alt="flutter-logo"
+            src="/flutterIcon.png"
+            width={52}
+            height={52}
+            className="w-[52px] h-[52px] lg:w-[77px] lg:h-[78px] "
+          />
+        </div>
+        {/* flutter heading */}
+        <div className="col-span-2">
+          <p className="text-5xl text-description">Flutter</p>
+        </div>
+        {/* description */}
+        <div className="col-span-2 lg:col-span-1">
+          <p className="text-description text-xl">
+            Our Flutter app development services are industry agnostic and our
+            team is skilled in using the framework to build applications for any
+            domain.
+          </p>
+        </div>
+        {/* USP */}
+        <div className="col-span-2 lg:col-span-1 text-description text-xl space-y-5">
+          <p>
+            USP 1 : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            et massa mi.
+          </p>
+          <p>
+            USP 1 : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            et massa mi.
+          </p>
+          <p>
+            USP 1 : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            et massa mi.
+          </p>
+        </div>
+      </div>
+
+      {/* Flutter */}
+      <div className="grid gap-5 lg:grid-cols-2">
+        {/* flutter logo */}
+        <div className="col-span-2">
+          <Image
+            alt="flutter-logo"
+            src="/flutterIcon.png"
+            width={52}
+            height={52}
+            className="w-[52px] h-[52px] lg:w-[77px] lg:h-[78px] "
+          />
+        </div>
+        {/* flutter heading */}
+        <div className="col-span-2">
+          <p className="text-5xl text-description">Flutter</p>
+        </div>
+        {/* description */}
+        <div className="col-span-2 lg:col-span-1">
+          <p className="text-description text-xl">
+            Our Flutter app development services are industry agnostic and our
+            team is skilled in using the framework to build applications for any
+            domain.
+          </p>
+        </div>
+        {/* USP */}
+        <div className="col-span-2 lg:col-span-1 text-description text-xl space-y-5">
+          <p>
+            USP 1 : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            et massa mi.
+          </p>
+          <p>
+            USP 1 : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            et massa mi.
+          </p>
+          <p>
+            USP 1 : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            et massa mi.
+          </p>
+        </div>
+      </div>
+
+      {/* Flutter */}
+      <div className="grid gap-5 lg:grid-cols-2">
+        {/* flutter logo */}
+        <div className="col-span-2">
+          <Image
+            alt="flutter-logo"
+            src="/flutterIcon.png"
+            width={52}
+            height={52}
+            className="w-[52px] h-[52px] lg:w-[77px] lg:h-[78px] "
+          />
+        </div>
+        {/* flutter heading */}
+        <div className="col-span-2">
+          <p className="text-5xl text-description">Flutter</p>
+        </div>
+        {/* description */}
+        <div className="col-span-2 lg:col-span-1">
+          <p className="text-description text-xl">
+            Our Flutter app development services are industry agnostic and our
+            team is skilled in using the framework to build applications for any
+            domain.
+          </p>
+        </div>
+        {/* USP */}
+        <div className="col-span-2 lg:col-span-1 text-description text-xl space-y-5">
+          <p>
+            USP 1 : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            et massa mi.
+          </p>
+          <p>
+            USP 1 : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            et massa mi.
+          </p>
+          <p>
+            USP 1 : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            et massa mi.
+          </p>
+        </div>
+      </div>
+
+      {/* Flutter */}
+      <div className="grid gap-5 lg:grid-cols-2">
+        {/* flutter logo */}
+        <div className="col-span-2">
+          <Image
+            alt="flutter-logo"
+            src="/flutterIcon.png"
+            width={52}
+            height={52}
+            className="w-[52px] h-[52px] lg:w-[77px] lg:h-[78px] "
+          />
+        </div>
+        {/* flutter heading */}
+        <div className="col-span-2">
+          <p className="text-5xl text-description">Flutter</p>
+        </div>
+        {/* description */}
+        <div className="col-span-2 lg:col-span-1">
+          <p className="text-description text-xl">
+            Our Flutter app development services are industry agnostic and our
+            team is skilled in using the framework to build applications for any
+            domain.
+          </p>
+        </div>
+        {/* USP */}
+        <div className="col-span-2 lg:col-span-1 text-description text-xl space-y-5">
+          <p>
+            USP 1 : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            et massa mi.
+          </p>
+          <p>
+            USP 1 : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            et massa mi.
+          </p>
+          <p>
+            USP 1 : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            et massa mi.
+          </p>
+        </div>
+      </div>
+
+      {/* Flutter */}
+      <div className="grid gap-5 lg:grid-cols-2">
+        {/* flutter logo */}
+        <div className="col-span-2">
+          <Image
+            alt="flutter-logo"
+            src="/flutterIcon.png"
+            width={52}
+            height={52}
+            className="w-[52px] h-[52px] lg:w-[77px] lg:h-[78px] "
+          />
+        </div>
+        {/* flutter heading */}
+        <div className="col-span-2">
+          <p className="text-5xl text-description">Flutter</p>
+        </div>
+        {/* description */}
+        <div className="col-span-2 lg:col-span-1">
+          <p className="text-description text-xl">
+            Our Flutter app development services are industry agnostic and our
+            team is skilled in using the framework to build applications for any
+            domain.
+          </p>
+        </div>
+        {/* USP */}
+        <div className="col-span-2 lg:col-span-1 text-description text-xl space-y-5">
+          <p>
+            USP 1 : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            et massa mi.
+          </p>
+          <p>
+            USP 1 : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            et massa mi.
+          </p>
+          <p>
+            USP 1 : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            et massa mi.
+          </p>
+        </div>
+      </div>
+
+      {/* Flutter */}
+      <div className="grid gap-5 lg:grid-cols-2">
+        {/* flutter logo */}
+        <div className="col-span-2">
+          <Image
+            alt="flutter-logo"
+            src="/flutterIcon.png"
+            width={52}
+            height={52}
+            className="w-[52px] h-[52px] lg:w-[77px] lg:h-[78px] "
+          />
+        </div>
+        {/* flutter heading */}
+        <div className="col-span-2">
+          <p className="text-5xl text-description">Flutter</p>
+        </div>
+        {/* description */}
+        <div className="col-span-2 lg:col-span-1">
+          <p className="text-description text-xl">
+            Our Flutter app development services are industry agnostic and our
+            team is skilled in using the framework to build applications for any
+            domain.
+          </p>
+        </div>
+        {/* USP */}
+        <div className="col-span-2 lg:col-span-1 text-description text-xl space-y-5">
+          <p>
+            USP 1 : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            et massa mi.
+          </p>
+          <p>
+            USP 1 : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            et massa mi.
+          </p>
+          <p>
+            USP 1 : Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+            et massa mi.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AboutTech;
