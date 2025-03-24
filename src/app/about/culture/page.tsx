5e0ea8e0d11b4d2c07a1072704ca2af3e3331a80
Akash <akash.v.harale@gmail.com>
2025-03-24 13:22:45 +0530

    changes as per instructed

--- src/app/about/culture/page.tsx
+++ src/app/about/culture/page.tsx
@@ -36,9 +36,9 @@ const AboutCulture = () => {
           Our culture is <br className="hidden lg:block" />
           important to us.
         </h1>
-        <div className=" grid gap-6 mt-14 grid-cols-2 lg:grid-cols-3 col-span-full lg:col-span-3  lg:mt-20 ">
-          <div className="space-y-4">
-            <div className="grid grid-cols-2">
+        <div className=" grid gap-6  mt-14 grid-cols-2 lg:grid-cols-3 col-span-full lg:col-span-3  lg:mt-20 ">
+          <div className="space-y-4 ">
+            <div className="lg:max-w-[95%] grid grid-cols-2">
               <p className="text-description/70 text-base lg:text-2xl col-span-1">
                 Collaboration
               </p>
@@ -55,7 +55,7 @@ const AboutCulture = () => {
           </div>
 
           <div className="space-y-4">
-            <div className="grid grid-cols-3">
+            <div className="lg:max-w-[95%] grid grid-cols-3">
               <p className="text-description/70 text-base lg:text-2xl col-span-2">
                 Efficiency and optimization
               </p>
@@ -72,7 +72,7 @@ const AboutCulture = () => {
           </div>
 
           <div className="space-y-4">
-            <div className="grid grid-cols-2">
+            <div className="lg:max-w-[95%] grid grid-cols-2">
               <p className="text-description/70 text-base lg:text-2xl col-span-1">
                 We like to have fun
               </p>
@@ -91,11 +91,11 @@ const AboutCulture = () => {
       </div>
 
       {/* Collaboration */}
-      <div className="grid  gap-2 lg:grid-cols-4 lg:gap-x-4">
+      <div className="grid gap-2 lg:grid-cols-4 lg:gap-x-4">
         <div className="col-span-2 ">
-          <div className="max-w-lg flex flex-col justify-between">
-            <p className="grid-heading-with-primary-color ">Collaboration</p>
-            <p className="text-description/70 mb-5">
+          <div className="max-w-lg flex flex-col justify-between  gap-5">
+            <p className="grid-heading-with-primary-color leading-none">Collaboration</p>
+            <p className="text-description/70 ">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et
               massa mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien
               fringilla, mattis ligula consectetur, ultrices mauris.
@@ -169,7 +169,7 @@ const AboutCulture = () => {
       {/* Efficiency and Optimization */}
       <div className="grid gap-2 grid-cols-2 lg:grid-cols-4 lg:gap-x-4">
         {/* 1st grid box */}
-        <div className="col-span-2 flex flex-col justify-between gap-y-2 row-start-1 row-end-2">
+        <div className=" col-span-2 flex flex-col justify-between gap-5 row-start-1 row-end-2">
           <p className="grid-heading-with-primary-color leading-none">
             Efficiency and Optimization
           </p>
@@ -227,11 +227,11 @@ const AboutCulture = () => {
       {/* Efficiency and Optimization */}
       <div className="grid gap-2 grid-cols-2 lg:grid-cols-4 lg:gap-x-4">
         {/* 1st grid box */}
-        <div className="col-span-2 flex flex-col justify-between gap-y-2 row-start-1 row-end-2">
-          <p className="grid-heading-with-primary-color leading-none">
-            We like to have <br className="hidden lg:block" /> fun
+        <div className="col-span-2 flex flex-col justify-between gap-5 row-start-1 row-end-2">
+          <p className="grid-heading-with-primary-color leading-none lg:max-w-[75%]">
+            We like to have  fun
           </p>
-          <p className="text-description/70 ">
+          <p className="text-description/70 lg:max-w-[75%]">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa
             mi. Aliquam in hendrerit urna. Pellentesque sit amet sapien
             fringilla, mattis ligula consectetur, ultrices mauris.
